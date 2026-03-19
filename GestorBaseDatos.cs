8f77a6df8dacdbb526bf9786cd0e626fe10cb5f1
Caleb Aguilar <jocalebaguilar@gmail.com>
2026-03-19 01:15:53 -0600

    clase BD: constructor, guardar cancion y crear playlist

---
+++ GestorBaseDatos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ReproductorMusica
+{
+    public class GestorBaseDatos
+    {
+        private string cadenaConexion;
+
+        //metodo constructor
+        public GestorBaseDatos(string conexion)
+        {
+            cadenaConexion = conexion;
+        }
+
+        //metodo para guardar una cancion
+        public void GuardarCancion(string nombre, string artista, string ruta)
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                conn.Open();
+
+                string query = "INSERT INTO Cancion (nombre, artista, rutaArchivo) VALUES (@nombre, @artista, @ruta)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@artista", artista);
+                cmd.Parameters.AddWithValue("@ruta", ruta);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        //metodo para crear playlist
+        public void CrearPlaylist(string nombrePlaylist)
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                conn.Open();
+
+                string query = "INSERT INTO Playlist (nombrePlaylist) VALUES(@nombre)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@nombre", nombrePlaylist);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
